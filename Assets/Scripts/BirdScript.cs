a2fc0d4d3683d4c4974402dd7037487d3c20cb7b
sam17332 <32148906+sam17332@users.noreply.github.com>
2018-01-17 09:07:55 -0600

    primer commit

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+	public float jumpForce = 35f;
+	public float forwardSpeed = 2f;
+	public Rigidbody2D rb;
+	public GameObject cam;
+	public bool dead = false;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (dead == false) {
+			if (Input.GetButtonDown ("Jump")) {
+				rb.velocity = Vector2.zero;
+				rb.AddForce (Vector2.up * jumpForce);
+			}
+			rb.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+			cam.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+		}
+		if (rb.transform.position.x>14)
+		{
+			dead = true;
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		dead = true;
+
+	}
+}
